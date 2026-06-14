40a4b956821484df2b6fea9e044036b34269b28f
PlexILIghT <Liexa2024@gmail.com>
2026-06-15 08:48:42 +1000

    change forgoren naming

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/style.css">
 </head>
-body>
+<body>
     <div class="container d-flex justify-content-center align-items-center vh-100">
         <div class="row">
             <div class="col-12 text-center">
-                <h1 class="mb-4">Login Init</h1>
+                <h1 class="mb-4">h4ck3r posts</h1>
                 <?php
                 if (!isset($_COOKIE['User'])){
                 ?>
